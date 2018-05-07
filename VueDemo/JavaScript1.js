e20ba579df20c4b1e2d4dc748ebe618cb9a65892
Sparks <dsparks@proforma.com>
2018-05-07 11:39:35 -0400

    added powerpoint and vue.js demo code

---
+++ VueDemo/JavaScript1.js
@@ -0,0 +1,13 @@
+﻿const app2 = new Vue({
+    el: '#app2',
+    data: {
+        characters: []
+    },
+    created() {
+        fetch('https://swapi.co/api/people')
+            .then(response => response.json())
+            .then(json => {
+                this.characters = json.characters
+            })
+    }
+})
